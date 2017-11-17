7b713c4db64613e7d98ecddd4339ea762b818b6e
micheal walsh <michealwalsh@micheals-MacBook-Pro.local>
2017-11-17 17:16:25 +0000

    updated smaples.html with projects

--- scripts/script.js
+++ scripts/script.js
@@ -96,3 +96,37 @@
 	});
 
 });
+
+//Smooth Scrolling
+$(document).ready(function(){
+  $("#button1").click(function(e) {
+    e.preventDefault();
+    $('html, body').animate({
+      scrollTop: $($.attr(this, 'href')).offset().top
+    }, 700);
+  });
+});
+$(document).ready(function(){
+  $("#button2").click(function(e) {
+    e.preventDefault();
+    $('html, body').animate({
+      scrollTop: $($.attr(this, 'href')).offset().top
+    }, 700);
+  });
+});
+$(document).ready(function(){
+  $("#button3").click(function(e) {
+    e.preventDefault();
+    $('html, body').animate({
+      scrollTop: $($.attr(this, 'href')).offset().top
+    }, 700);
+  });
+});
+$(document).ready(function(){
+  $("#button4").click(function(e) {
+    e.preventDefault();
+    $('html, body').animate({
+      scrollTop: $($.attr(this, 'href')).offset().top
+    }, 700);
+  });
+});
